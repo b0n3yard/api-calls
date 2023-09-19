dfaacfc01f55a32efe91f2a31e7ee7e96c01dca1
b0n3yard <namesake39@gmail.com>
2023-09-19 15:55:23 -0400

    api 1 call connected

---
+++ assets/js/scripts.js
@@ -0,0 +1,22 @@
+var quote = $('.stuff')
+var muscle = 'biceps'
+$.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/exercises?muscle=' + muscle,
+    headers:{'X-Api-Key': 'bQQv3qWq5kHalNjh19VAPQ==OdFpPSD3HrbB6vz5'},
+    contentType: 'application/json',
+    success: function(result) {
+        console.log(result[0]);
+        quote.append('<p>' + result[0].name+'</p>');
+        quote.append('<h2>' + result[0].difficulty + '</h2>');
+        quote.append('<p>' + result[0].instructions + '</p>');
+        
+    },
+    });
+// $.get('https://api.api-ninjas.com/v1/exercises?muscle=biceps')
+//     .then(function(data){
+//         var hi = data.content;
+//         console.log(data)
+
+//         quote.append(hi)
+//     });
